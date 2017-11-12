065305db81b8560cf53a99e0e8668bc7ba61820d
Guillermo <guillermo@Guillermos-Mac.local>
2017-11-11 20:31:29 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,44 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/index.paginas';
+
+import { AjustesService } from '../providers/ajustes';
+
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+export class MyApp {
+
+  rootPage:any;
+
+  constructor(private platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+              private _ajustes:AjustesService) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+
+      this._ajustes.cargar_storage()
+          .then( ()=>{
+
+            if( this._ajustes.ajustes.mostrar_tutorial ){
+              this.rootPage = "Introduccion";
+            }else{
+              this.rootPage = TabsPage;
+            }
+
+              this.platform.pause.subscribe( ()=> {
+                console.log("La aplicacion se detendra");
+              });
+
+              this.platform.resume.subscribe( ()=>{
+                console.log("La aplicacion va a continuar");
+              })
+
+              statusBar.styleDefault();
+              splashScreen.hide();
+            })
     });
   }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { HomePage, GuardadosPage, TabsPage, LoginPage } from '../pages/index.paginas';
+
+import { IonicStorageModule } from '@ionic/storage';
+
+//servicios
+import { AjustesService } from '../providers/ajustes';
+import { HistorialService } from '../providers/historial';
+
+//plugins
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Contacts } from '@ionic-native/contacts';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { UsuarioService } from '../providers/usuario';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    GuardadosPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    GuardadosPage,
+    HomePage,
+    LoginPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    BarcodeScanner,
+    InAppBrowser,
+    Contacts,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AjustesService,
+    HistorialService,
+    UsuarioService
   ]
 })
 export class AppModule {}
